5e708911c72878fea880d805f089aa06616013da
antoineBarbez <antoine.brbz@gmail.com>
2020-08-05 10:44:12 +0100

    use get_layer function of keras.Model to select the right output layer of ResNet

--- models/feature_extractor.py
+++ models/feature_extractor.py
@@ -6,9 +6,6 @@ def get_feature_extractor_model(image_shape):
     x = tf.cast(i, tf.float32)
     x = tf.keras.applications.resnet.preprocess_input(x)
     resnet_50 = tf.keras.applications.ResNet50(include_top=False, weights="imagenet", input_shape=image_shape)
-    for j, layer in enumerate(resnet_50.layers):
-        if layer.name == "conv4_block6_out":
-            index = j
-    core = tf.keras.Model(inputs=resnet_50.input, outputs=resnet_50.layers[index].output)
+    core = tf.keras.Model(inputs=resnet_50.input, outputs=resnet_50.get_layer("conv4_block6_out").output)
     x = core(x)
     return tf.keras.Model(inputs=[i], outputs=[x])
